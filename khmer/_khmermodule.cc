3b900979484a8f84b7a48a14195b897ebec25d0b
Luiz Irber <luiz.irber@gmail.com>
2014-04-03 18:57:22 -0400

    Working with SHA-1

--- khmer/_khmermodule.cc
+++ khmer/_khmermodule.cc
@@ -125,7 +125,8 @@ private:
     std::string _message;
 public:
     _khmer_exception(std::string message) : _message(message) { };
-    inline const std::string get_message() const {
+    inline const std::string get_message() const
+    {
         return _message;
     };
 };
